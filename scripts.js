9be2baccf55370052445750fdcccde3b1a35b184
Chris Foster <fosterchris22@gmail.com>
2017-02-26 12:45:44 -0500

    Populate board and place robot and kitty.

--- scripts.js
+++ scripts.js
@@ -7,41 +7,55 @@ var levelOne = {
 }
 
 var levelTwo = {
-	kittyPlacement: [0, 1],
+	kittyPlacement: [1, 1],
 	robotPlacement: [4, 4],
 	board: [['empty', 'empty', 'empty'], ['empty', 'empty', 'empty'], ['empty', 'empty', 'empty', 'empty', 'empty'], ["offset 2", "offset-2", "empty", "empty", "empty"], ["offset 2", "offset-2", "empty", "empty", "empty"]]
 }
 
 var levels = [levelOne, levelTwo];
 
-function Game(levels) {
+function Game(levels, robot, kitty) {
 	this.levels = levels;
 	this.start = start;
 	this.generateBoard = generateBoard;
 
 	function start() {
-		generateBoard(levels[1].board);
+		generateBoard(levels[1]);
+		// place(robot);
+		// place(kitty);
 	}
 
-	function generateBoard(mapArray) {
-		for (var i = 0; i < mapArray.length; i++) {
-			var rowContainer = document.createElement('div');
-			rowContainer.classList.add('row-container');
+	function generateBoard(level) {
+		for (var i = 0; i < level.board.length; i++) {
 			var row = document.createElement('div');
 			row.classList.add('row');
-			// board.appendChild(rowContainer);
 			board.appendChild(row);
-			generateRow(mapArray[i], row);
+			generateRow(level.board[i], row);
 		}
+
+		[robot, kitty].map(function(robotOrKitty) {
+			place(robotOrKitty, level);
+		});
+
+	}
+
+	function place(robotOrKitty, level) {
+		robotOrKitty.coords = level[robotOrKitty.name + "Placement"];
+		var placeRow = robotOrKitty.coords[0];
+		var placeColumn = robotOrKitty.coords[1];
+		var robotOrKittyDiv = document.createElement("div");
+		robotOrKittyDiv.classList.add(robotOrKitty.name);
+		document.getElementsByClassName("row")[placeRow].getElementsByTagName("div")[placeColumn].append(robotOrKittyDiv);
 	}
 
 	function generateRow(innerMapArray, row) {
 		var splitString = innerMapArray[0].split(" ");
 		if (splitString[0] === "offset") {
 			row.classList.add("offset-" + splitString[1]);
-			for (var i = 0; i <= splitString.length; i++) {
-				var empty = document.createElement("div");
-				empty.classList.add("space");
+			for (var i = 0; i < splitString.length; i++) {
+				var space = document.createElement("div");
+				space.classList.add("space");
+				row.append(space);
 			}
 		}
 		for (var i = splitString[1] || 0; i < innerMapArray.length; i++) {
@@ -52,6 +66,17 @@ function Game(levels) {
 	}
 }
 
-var myGame = new Game(levels);
+function Robot() {
+	this.name = "robot";
+}
+
+function Kitty() {
+	this.name = "kitty";
+}
+
+var robot = new Robot();
+var kitty = new Kitty();
+
+var myGame = new Game(levels, robot, kitty);
 
 myGame.start();
